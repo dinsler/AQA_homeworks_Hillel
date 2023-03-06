fc6899c0e300f84dba0837450f307dd713064b6d
dinsler <hanna.dinsler@gmail.com>
2023-03-07 00:56:49 +0200

    updated solution with dictionary fromkeys method

--- hw2_git_workflow_sequences/homework2_task6.py
+++ hw2_git_workflow_sequences/homework2_task6.py
@@ -4,9 +4,12 @@
     If there are 2 johns in the list, you need to take only one of them.
     "John Dow", "John Dow", "Marta Dow" => "John Dow", "Marta Dow ")
 """
+# Solution with fromkeys method
 non_unique_names = ['Bob', 'Marley', 'Genna', 'Michael', 'Bob', 'Genna']
-unique_names = []
-for name in non_unique_names:
-    if name not in unique_names:
-        unique_names.append(name)
-print(unique_names)
+unique_names = {}.fromkeys(non_unique_names, None)
+print(unique_names.keys())
+
+# for name in non_unique_names:
+#     if name not in unique_names:
+#         unique_names.append(name)
+# print(unique_names)
